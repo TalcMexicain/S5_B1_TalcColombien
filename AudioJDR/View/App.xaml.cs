b48283cac0327a369d4c967d87cdff29215b6c6a
Aaron Gritti :) <aaron.gritti@iut-dijon.u-bourgogne.fr>
2024-10-07 16:07:22 +0200

    Project Setup
    Simple setup, added multilingual support.
    English and French are available and easily swappable via the code.

---
+++ AudioJDR/View/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace View
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //SetCulture("en"); // To test in other languages
+
+            MainPage = new AppShell();
+        }
+
+        /// <summary>
+        /// Changes the language of the app
+        /// </summary>
+        /// <param name="cultureCode">Country code of wanted language (e.g. 'fr')</param>
+        public void SetCulture(string cultureCode)
+        {
+            CultureInfo culture = new CultureInfo(cultureCode);
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+        }
+    }
+}
---
+++ AudioJDR/View/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace View
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
