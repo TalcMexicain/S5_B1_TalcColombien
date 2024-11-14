ddc58f819ae2c30d8f65c3b58d66805251c7247c
Dorian-vivot <dorian.vivot@iut-dijon.u-bourgogne.fr>
2024-11-14 14:11:06 +0100

    Add a new Unitary test for Option Class

--- AudioJDR/UnitTests/OptionTests.cs
+++ AudioJDR/UnitTests/OptionTests.cs
@@ -42,5 +42,73 @@ namespace UnitTests
 
             Assert.Equal(eventSample, optionToTest.LinkedEvent);
         }
+
+        [Fact]
+        public void Option_AddWordInList_Test()
+        {
+            Option optionToTest = new Option();
+            optionToTest.NameOption = "Test Option";
+
+            string wordToAdd = "Test Word";
+            optionToTest.AddWordInList(wordToAdd);
+
+            Assert.Contains(wordToAdd, optionToTest.GetWords());
+        }
+
+        [Fact]
+        public void Option_GetWord_Test()
+        {
+            Option optionToTest = new Option();
+
+            string wordToAdd = "Test Word";
+            string wordToAdd2 = "Test Word 3";
+
+            optionToTest.AddWordInList(wordToAdd);
+            optionToTest.AddWordInList(wordToAdd2);
+
+            List<string> wordsListToTest = optionToTest.GetWords();
+
+            Assert.NotNull(wordsListToTest);
+            Assert.Equal(2, wordsListToTest.Count);
+            Assert.Contains(wordToAdd, wordsListToTest);
+        }
+
+        [Fact]
+        public void Option_RemoveWordInList_Test()
+        {
+            Option optionToTest = new Option();
+
+            string wordToAdd = "Test Word";
+
+            optionToTest.AddWordInList(wordToAdd);
+            Assert.Contains(wordToAdd, optionToTest.GetWords());
+
+            optionToTest.RemoveWordInList(wordToAdd);
+
+            List<string> wordsListToTest = optionToTest.GetWords();
+
+            Assert.DoesNotContain(wordToAdd, wordsListToTest);
+            Assert.Empty(wordsListToTest);
+        }
+
+        //public bool IsWordsListNotEmpty()
+        //{
+        //    return this.words != null && this.words.Any();
+        //}
+
+        [Fact]
+        public void Option_IsWordsListNotEmpty_Test()
+        {
+            Option optionToTest = new Option();
+
+            bool testIsListEmpty = optionToTest.IsWordsListNotEmpty();
+            Assert.False(testIsListEmpty);
+
+            string wordToAdd = "Test Word";
+            optionToTest.AddWordInList(wordToAdd);
+            
+            bool testIsListNotEmpty = optionToTest.IsWordsListNotEmpty();
+            Assert.True(testIsListNotEmpty);
+        }
     }
 }
