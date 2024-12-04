28e42d10a7929287955d6f9f9066ed9fd3fbf974
Dorian-vivot <dorian.vivot@iut-dijon.u-bourgogne.fr>
2024-12-04 11:13:28 +0100

    New optionViewModel unit tests

--- AudioJDR/TestViewModel/TestOptionVM.cs
+++ AudioJDR/TestViewModel/TestOptionVM.cs
@@ -10,18 +10,16 @@ namespace TestViewModel
 {
     public class TestOptionVM
     {
-        private StoryViewModel _storyVM;
+        private FakeStoryManager _fakeStoryManager;
+        private StoryViewModel _storyViewModel;
         private EventViewModel _eventVM;
 
-        public TestOptionVM() 
+        public TestOptionVM()
         {
-            Story story = new Story();
-            story.IdStory = 1;
-            story.AddEvent(new Event());
-
-            _storyVM = new StoryViewModel();
-            _storyVM.CurrentStory = story;
-            _eventVM = new EventViewModel(_storyVM);
+            _fakeStoryManager = new FakeStoryManager();
+            _storyViewModel = new StoryViewModel(_fakeStoryManager);
+            InitializeStoryVMAndEventVM();
+            _eventVM = new EventViewModel(_storyViewModel);
         }
 
         [Fact]
@@ -53,11 +51,130 @@ namespace TestViewModel
         }
 
         [Fact]
-        public async Task UpdateOptionAsync_NullException()
+        public async void UpdateOptionAsync_Test()
+        {
+            OptionViewModel optionVM = new OptionViewModel(_eventVM);
+
+            string updatedName = "Updated Option";
+            Option updateOption = new Option()
+            {
+                NameOption = updatedName,
+                LinkedEvent = new Event(){ IdEvent = 1, Name = "Linked Event"}
+            };
+
+            await optionVM.UpdateOptionAsync(updateOption);
+
+            Assert.Equal(updatedName, optionVM.CurrentOption.NameOption);
+            Assert.NotNull(optionVM.CurrentOption.LinkedEvent);
+            Assert.Equal("Linked Event", optionVM.CurrentOption.LinkedEvent.Name);
+        }
+
+        [Fact]
+        public async Task UpdateOptionAsync_TestNullException()
         {
             OptionViewModel optionVM = new OptionViewModel(_eventVM);
 
             await Assert.ThrowsAsync<ArgumentNullException>(() => optionVM.UpdateOptionAsync(null));
+        }
+
+        [Fact]
+        public async void InitializeNewOptionAsync()
+        {
+            OptionViewModel optionVM = new OptionViewModel(_eventVM);
+
+            await optionVM.InitializeNewOptionAsync();
+
+            Assert.NotNull(optionVM.CurrentOption);
+            Assert.Single(_eventVM.Options);
+            Assert.Equal(string.Empty, optionVM.CurrentOption.NameOption);
+            Assert.Null(optionVM.CurrentOption.LinkedEvent);
+            Assert.Equal(optionVM.GenerateNewOptionId() - 1, optionVM.CurrentOption.IdOption);
+        }
+
+        [Fact]
+        public async void DeleteAsync_Test()
+        {
+            OptionViewModel optionVM = new OptionViewModel(_eventVM);
+
+            Option optionToDelete = new Option()
+            {
+                IdOption = optionVM.GenerateNewOptionId(),
+                NameOption = "Option",
+                LinkedEvent = null
+            };
+
+            await _eventVM.AddOptionAsync(optionToDelete);
+
+            Assert.Single(_eventVM.Options);
+
+            await optionVM.DeleteAsync();
+            Assert.Empty(_eventVM.Options);
+        }
+
+        [Fact]
+        public async void AddWordAsync_Test()
+        {
+            OptionViewModel optionVM = new OptionViewModel(_eventVM);
+
+            string wordToAdd = "NewWord";
+            bool result = await optionVM.AddWordAsync(wordToAdd);
+
+            Assert.True(result);
+            Assert.Contains(wordToAdd, optionVM.Words);
+        }
+
+        [Fact]
+        public async void AssWordAsync_TestExistingWord()
+        {
+            OptionViewModel optionVM = new OptionViewModel(_eventVM);
+
+            string wordToAddTwice = "WordToAdd";
+            await optionVM.AddWordAsync(wordToAddTwice);
+
+            bool result = await optionVM.AddWordAsync(wordToAddTwice);
+
+            Assert.False(result);
+            Assert.Single(optionVM.Words);
+        }
+
+        [Fact]
+        public async void RemoveWordAsync_Test()
+        {
+            OptionViewModel optionVM = new OptionViewModel(_eventVM);
+
+            string wordToRemove = "Word";
+            await optionVM.AddWordAsync(wordToRemove);
+
+            bool result = await optionVM.RemoveWordAsync(wordToRemove);
+
+            Assert.True(result);
+            Assert.Empty(optionVM.Words);
+        }
+
+        [Fact]
+        public async void RemoveWordAsync_TestNotFoundWord()
+        {
+            OptionViewModel optionVM = new OptionViewModel(_eventVM);
+
+            string wordToRemove = "Word";
+
+            bool result = await optionVM.RemoveWordAsync(wordToRemove);
+
+            Assert.False(result);
+            Assert.Empty(optionVM.Words);
+        }
+
+        private async void InitializeStoryVMAndEventVM()
+        {
+            Story story = new Story()
+            {
+                IdStory = 1,
+                Title = "Title",
+                Description = "Description"
+            };
+
+            _storyViewModel.CurrentStory = story;
+            await _storyViewModel.AddStoryAsync(story);
         }
     }
 }
